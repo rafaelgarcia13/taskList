be2b202a1f9ac008387600da51d62252f6e510c1
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-06-04 16:53:12 -0300

    feat: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Button, ScrollView, StatusBar, Text, useColorScheme } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { ThemeProvider } from 'styled-components/native';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-import TrendingMoviesService from './services/toDoListService/ToDoListService';
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBarStyled } from './components/statusBar/StatusBar';
+import { NavigatorContainer } from './routes/NavigatorContainer';
+import { useStore } from './store/useStore';
+
 import { SafeAreaView } from './styles/global';
-import { darkTheme } from './styles/themes/dark';
-import { lightTheme } from './styles/themes/light';
-import { ThemesIds } from './styles/themes/themeModel';
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
-  const [theme, setTheme] = useState(lightTheme);
-
-  const toggleTheme = () =>
-    setTheme((previousTheme) =>
-      previousTheme.id === ThemesIds.dark ? lightTheme : darkTheme,
-    );
-
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const teste = async () => {
-    const data = await TrendingMoviesService.getAllToDos();
-    console.log('🚀 ~ file: App.tsx ~ line 29 ~ teste ~ data', JSON.stringify(data));
-    toggleTheme();
-  };
-
-  useEffect(() => {
-    teste();
-  }, []);
+  const theme = useStore(({ theme }) => theme);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <ThemeProvider theme={theme}>
-        <StatusBar barStyle={'dark-content'} backgroundColor={Colors.lighter} />
-        <ScrollView>
-          <Text>Ola mundo</Text>
-          <Button onPress={toggleTheme} title="Trocar tema" />
-        </ScrollView>
-      </ThemeProvider>
+    <SafeAreaView>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <StatusBarStyled theme={theme} />
+          <NavigatorContainer />
+        </ThemeProvider>
+      </QueryClientProvider>
     </SafeAreaView>
   );
 };
