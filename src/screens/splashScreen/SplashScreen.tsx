f17eda0948fe8a90f60f38aed2b665da3cec89d6
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-06-04 09:45:47 -0300

    feat: splash screen

---
+++ src/screens/splashScreen/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SplashScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Carregando</Text>
+    </View>
+  );
+};
+
+export { SplashScreen };
