da5908733756cb70d3966352c22e9a43ec2515e1
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-06-04 22:08:55 -0300

    feat: loading receiving size by props

--- src/components/spinnerLoading/SpinnerLoading.tsx
+++ src/components/spinnerLoading/SpinnerLoading.tsx
@@ -5,19 +5,21 @@ import { LoadingContainer } from './styles';
 
 interface ISpinnerLoadingProps {
   marginTop?: number;
+  size?: number | 'large' | 'small' | undefined;
 }
 
-const SpinnerLoading: React.FC<ISpinnerLoadingProps> = ({ marginTop }) => {
+const SpinnerLoading: React.FC<ISpinnerLoadingProps> = ({ marginTop, size }) => {
   const theme = useTheme();
   return (
     <LoadingContainer marginTop={marginTop}>
-      <ActivityIndicator size="large" color={theme.colors.primary} />
+      <ActivityIndicator size={size} color={theme.colors.primary} />
     </LoadingContainer>
   );
 };
 
 SpinnerLoading.defaultProps = {
   marginTop: 0,
+  size: 'large',
 };
 
 export { SpinnerLoading };
