d76fb4cc113e9f8aab0bc7f192aa59810a062860
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-06-05 00:24:53 -0300

    feat: new store to open feedback card

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { IFeedbackCard } from '../components/feedbackCard/feedbackCardModel';
 import AsyncStorageFacade from '../lib/localStorage/AsyncStorageFacade';
 import { themeInfoKey } from '../lib/localStorage/localStorageKeys';
 import { lightTheme } from '../styles/themes/light';
@@ -7,14 +8,17 @@ import { IThemeModel } from '../styles/themes/themeModel';
 interface StoreState {
   userName: string;
   theme: IThemeModel;
+  feedbackData: IFeedbackCard;
 }
 
 interface StoreActions {
   saveUserName: (user: string) => void;
   setTheme: (theme: IThemeModel) => void;
+  setFeedback: (feedback: IFeedbackCard) => void;
 }
 
 const useStore = create<StoreState & StoreActions>((set) => ({
+  feedbackData: { isError: false, message: '', isOpen: false },
   theme: lightTheme,
   userName: '',
   setTheme: (theme: IThemeModel) => {
@@ -22,6 +26,7 @@ const useStore = create<StoreState & StoreActions>((set) => ({
     AsyncStorageFacade.setItem<IThemeModel>(themeInfoKey, theme);
   },
   saveUserName: (userName: string) => set({ userName }),
+  setFeedback: (feedbackData: IFeedbackCard) => set({ feedbackData }),
 }));
 
 export { useStore };
